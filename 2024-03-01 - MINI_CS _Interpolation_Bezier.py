53d4d2707b3c0ea6d3bcfb585d647198321de4b1
Matts-db <165037492+Matts-db@users.noreply.github.com>
2024-03-26 14:14:55 +0100

    Add files via upload

---
+++ 2024-03-01 - MINI_CS _Interpolation_Bezier.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  1 14:28:49 2024
+
+@author: Alain ETIENNE, Arts et Métiers - Campus de Metz
+"""
+
+
+import math
+
+def f(a : float, t : float) -> tuple[float]:
+    return a*(math.cos(t) + math.sin(t)), a*(math.sin(t)+t*math.cos(t))
+
+def echantillonner(f, nb_point : int) -> list[tuple]:
+    resultX=[1]
+    resultY=[0]
+    for t in range (1,nb_point,1/nb_point):
+        resultX.append(f(1,t)[0])
+        resultY.append(f(1,t)[1])
+    return resultX,resultY
+
+        
+    """
+    Fonction qui retourne l'ensemble de points d'interpolation, construit sur la fonction f passée en argument
+
+    Parameters
+    ----------
+    f : Pointeur de fonction
+        Fonction que l'on souhaiter interpoler
+    nb_point : int
+        Nombre de points d'interpolation
+
+    Returns
+    -------
+    list[tuple]
+        Liste contenant, sous la forme de tuple, l'ensemble des points d'interpolation
+
+    """
+    
+    # TODO : A compléter, puis retirer l'instruction pass
+    pass
+
+def determiner_poles(points_interpolation : list[tuple]) -> list[tuple]:
+    
+    
+    
+    """
+    Fonction qui calcule, puis de retourne les pôles de la courbe de Bézier en fonction d'une liste de points d'interpolation
+
+    Parameters
+    ----------
+    points_interpolation : list
+        Liste contenant les positions (sous forme de tuple) des points d'interpolation
+
+    Returns
+    -------
+    list
+        Liste contenant les positions (sous forme de tuple) des poles de la courbe de Bezier
+
+    """
+    # TODO : A compléter, puis retirer l'instruction pass
+    pass
+
+def calculer_points_courbe_Bezier(liste_poles : list[tuple], nb_points : int) -> list[tuple]:
+    """
+    Fonction qui retourne les coordonnées des points de la courbe de Bezier, construits sur les poles passés en argument
+
+    Parameters
+    ----------
+    liste_poles : list[tuple]
+        Liste contenant, sous la forme de tuple, l'ensemble des poles de la courbe de Bezier
+    nb_points : int
+        Nombre de points équirépartis de la courbe de Bezier souhaités
+
+    Returns
+    -------
+    list[tuple]
+        DESCRIPTION.
+
+    """   
+    # TODO : A compléter, puis retirer l'instruction pass
+    pass   
+
+def afficher_courbe(liste_points : list[tuple]) -> None:
+    """
+    Procédure d'affichage d'une courbe connue de manière discréte (par un ensemble de points)
+
+    Parameters
+    ----------
+    liste_points : list[tuple]
+        Points connus de la fonction (ou courbe) que l'on sohaite afficher
+
+    Returns
+    -------
+    None
+        Aucun retour n'est souhaité, uniquement l'affichage via matplotlib
+
+    """
+    # TODO : A compléter, puis retirer l'instruction pass
+    pass
+
+def afficher_erreur(nb_point_comparaison : int) -> list[float]:
+    result=[]
+    for i in range(nb_point):
+        
+    
+    
+    """
+    Fonction qui retourne, pour un nombre de points identique et équirépartis dans leurs domaines, l'erreur commise (la distance) entre la courbe de référenc et la courbe d'interpolation de Bezier
+
+    Parameters
+    ----------
+    nb_point_comparaison : int
+        Nombre de points de comparaison.
+
+    Returns
+    -------
+    list[float]
+        Liste contenant les écarts entre les points de la courbe d'origine et de la courbe d'interpolation de Bezier
+
+    """    
+    # TODO : A compléter, puis retirer l'instruction pass
+    pass
